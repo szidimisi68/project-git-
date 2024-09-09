caedc6f36c78af85333685ec73e76667de0f3998
Szidor Mihály <szidor.mihaly@burg.lo.hu>
2024-09-09 14:28:20 +0200

    Util fucntion factorial

--- function.js
+++ function.js
@@ -2,6 +2,15 @@ function getFirstElement (arr){
     return arr[0];
 }
 
+function factorial(szam){
+    var valasz = 1;
+    for (let index = 1; index < szam; index++) {
+        valasz *= index;
+        
+    }
+    return valasz;
+}
+
 const numbers = [1, 2, 3];
 const words = ["apple", "banana", "cherry"];
 
