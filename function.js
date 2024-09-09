7e54cd5fe75355ba7d0a9ed3578b6c25969c6437
Szidor Mihály <szidor.mihaly@burg.lo.hu>
2024-09-09 14:23:41 +0200

    first element

---
+++ function.js
@@ -0,0 +1,9 @@
+function getFirstElement (arr){
+    return arr[0];
+}
+
+const numbers = [1, 2, 3];
+const words = ["apple", "banana", "cherry"];
+
+console.log(`First number: ${getFirstElement(numbers)}`)
+console.log(`First word: ${getFirstElement(words)}`)
